6ef54fb3ed68e3d604b0c51e94f5f17a7faa6d97
David Gaspar <davidgaspar.dev@gmail.com>
2021-11-05 08:37:58 -0300

    Project started with "Hello, World!"

---
+++ app/src/main/java/dev/davidgaspar/studentlist/MainActivity.java
@@ -0,0 +1,17 @@
+package dev.davidgaspar.studentlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toast.makeText(this, "Hello, World!", Toast.LENGTH_LONG).show();
+    }
+}
