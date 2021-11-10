a247a7afdf58cdb1e991b3a0e247f71b3e74b7a9
David Gaspar <davidgaspar.dev@gmail.com>
2021-11-10 14:45:23 -0300

    Listing students as TextView in the LinearLayout

--- app/src/main/java/dev/davidgaspar/studentlist/MainActivity.java
+++ app/src/main/java/dev/davidgaspar/studentlist/MainActivity.java
@@ -1,12 +1,18 @@
 package dev.davidgaspar.studentlist;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+
+    private LinearLayout container;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -14,6 +20,24 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Hello, World!", Toast.LENGTH_LONG).show();
+        container = findViewById(R.id.main_activity_container);
+
+        List<String> students = new ArrayList<>(Arrays.asList(
+                "David Gaspar",
+                "Luiz Gustavo",
+                "Julia Herman",
+                "Julia Eirrof",
+                "Raul Zerefino",
+                "Eugênio Sousa",
+                "Ana Paula Martins",
+                "Renan Silveira",
+                "Jessica Sousa"
+        ));
+
+        for (int i = 0; i < students.size(); i++) {
+            TextView studentView = new TextView(this);
+            studentView.setText(students.get(i));
+            container.addView(studentView);
+        }
     }
 }
