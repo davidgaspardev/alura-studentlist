cfa93d46b4ba85dfa653adcff4d94401b13bdc55
David Gaspar <davidgaspar.dev@gmail.com>
2021-11-27 01:45:16 -0300

    Create test students on the Application layer

---
+++ app/src/main/java/dev/davidgaspar/studentlist/StudentListApplication.java
@@ -0,0 +1,36 @@
+package dev.davidgaspar.studentlist;
+
+import android.app.Application;
+
+import dev.davidgaspar.studentlist.model.Student;
+import dev.davidgaspar.studentlist.repository.StudentRepo;
+
+public class StudentListApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createTestStudents();
+    }
+
+    private void createTestStudents() {
+        StudentRepo studentRepo = new StudentRepo();
+
+        studentRepo.save(new Student(
+                "David Gaspar",
+                "48984596882",
+                "davidgaspar.dev@gmail.com"
+        ));
+        studentRepo.save(new Student(
+                "Elizabete Valda Corrêa",
+                "48984272584",
+                "betthcorrea@gmail.com"
+        ));
+        studentRepo.save(new Student(
+                "Smauel Gaspar",
+                "48984120118",
+                "gasparmoveis@gmail.com"
+        ));
+    }
+}
